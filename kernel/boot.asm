9f1611de70ad40ace9348f95147caecc8a86d2b0
A Stevens <146134930+AStevens-Ai@users.noreply.github.com>
2026-03-18 23:40:10 -0400

    added a20 check

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -17,37 +17,35 @@ _start:
     mov sp, 0xFFFF      ;move into stack pointer the offset at which the top of the stack will be
 
     ;load One sector width of sector 2 on cyl 0 head 0, on first HardDrive.
-    mov dl, [BootDrive]
-    mov ah, 0x02
-    mov al, 0x01
-    mov ch, 0x00
-    mov cl, 0x02
-    mov dh, 0x00
-    mov bx, 0x1000
+    ; mov dl, [BootDrive]
+    ; mov ah, 0x02
+    ; mov al, 0x01
+    ; mov ch, 0x00
+    ; mov cl, 0x02
+    ; mov dh, 0x00
+    ; mov bx, 0x1000
 
-    mov ax, 0x0000
-    mov es, ax
+    ;mov ax, 0x0000
+    ;mov es, ax
 
-    int 0x13    ;call bios int
+    ;int 0x13    ;call bios int
     
-    jc disk_error       ;if carryflag, jumping to disk error. Means error.
+    ;jc disk_error       ;if carryflag, jumping to disk error. Means error.
     
+    call check_a20
+    cmp ax, 1
 
     mov ax, 0xB800      ;setting vram segment base address in ax to write to later
     mov es, ax          ;set extra segment to vram segment address
-    mov bl, 0x0A        ;set al to black bg, green text fallout style to pass to print_string
+    mov bl, 0x0A        ;set bl to black bg, green text fallout style to pass to print_string
+    jne a20_off_print
     mov si, boot_string
     call print_string
 
     jmp exit
 
-disk_error:
-    mov ax, 0xB800      ;setting vram segment base address in ax to write to later
-    mov es, ax          ;set extra segment to vram segment address
-    mov si, disk_read_error ;load disk_read_error message in
-    mov bl, 0x04        ;set text color to dark red
-    call print_string   ;
-    jmp exit
+a20_off_print:
+    mov si, a20_not_ena_print
 
 print_string:
 .next_char:
@@ -63,8 +61,69 @@ done:
 exit:
     hlt
 
+disk_error:
+    mov ax, 0xB800      ;setting vram segment base address in ax to write to later
+    mov es, ax          ;set extra segment to vram segment address
+    mov si, disk_read_error ;load disk_read_error message in
+    mov bl, 0x04        ;set text color to dark red
+    call print_string   ;
+    jmp exit
+
+
+check_a20:
+    ; returns 1 in ax register if a20 is enabled and 0 if not enabled
+    pushf
+    push ds
+    push es
+    push di
+    push si
+
+    cli
+    ; xor turns any bit thats not the same as the others to 1
+    xor ax, ax ; ax = 0
+    mov es, ax 
+
+    not ax ; ax = 0xFFFF
+    mov ds, ax 
+
+    mov di, 0x0500
+    mov si, 0x0510
+
+    mov al, byte [es:di]
+    push ax
+
+    mov al, byte [ds:si]
+    push ax
+
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+
+    cmp byte [es:di], 0xFF
+
+    pop ax
+    mov byte [ds:si], al
+
+    pop ax
+    mov byte [es:di], al
+
+    mov ax, 0
+    je check_a20_exit
+
+    mov ax, 1
+check_a20_exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+
+    ret
+
 boot_string:
-    db "Booting into kernel later", 0;
+    db "A20 line enabled already.", 0;
+
+a20_not_ena_print:
+    db "A20 line is disabled, enable please.", 0;
 
 disk_read_error:
     db "Disk Read Error. Halting.", 0;
